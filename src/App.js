7ef7a3e0a2f524061a437c4cfb96eb2b2f17ed03
jdelobel5987 <delobel.julien@outlook.com>
2025-07-20 17:52:12 +0200

    remove useless content from the create-react-app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './assets/logo.svg';
 import './styles/App.css';
 import SearchContacts from './components/SearchContacts';
 import DisplayContacts from './components/DisplayContacts';
@@ -7,20 +6,7 @@ import DisplayContacts from './components/DisplayContacts';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
       <main>
         <h1>Welcome to the Family Registry App</h1>
         <p>This app helps you manage your family records efficiently.</p>
