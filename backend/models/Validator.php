6d9d91a1684bc1f317a22b693aa75f9b79a3eb53
jdelobel5987 <delobel.julien@outlook.com>
2025-08-04 01:19:19 +0200

    created a validator class model for registration inputs

---
+++ backend/models/Validator.php
@@ -0,0 +1,34 @@
+<?php
+
+class Validator {
+
+    public static function isNotEmpty(...$fields): bool {
+        foreach ($fields as $field) {
+            if (empty($field)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Validate username
+    public static function isValidUsername(string $username): bool {
+        return preg_match('/^[a-zA-Z0-9_]{3,20}$/', $username);
+    }
+
+    // Validate email
+    public static function isValidEmail(string $email): bool {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    // Validate password
+    public static function isStrongPassword(string $password): bool {
+        // at least 8 characters, including one uppercase letter, one lowercase letter, one digit, and one special character
+        return preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&#]{8,}$/', $password);
+    }
+
+
+    public static function passwordsMatch(string $password, string $repPassword): bool {
+        return $password === $repPassword;
+    }
+}
