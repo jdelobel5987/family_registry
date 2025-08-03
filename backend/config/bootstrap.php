d8609008aad4b1c840b4b92127b07f25bac27bff
jdelobel5987 <delobel.julien@outlook.com>
2025-08-04 01:23:35 +0200

    edit the php bootstrap config file to properly target the .env to be read

--- backend/config/bootstrap.php
+++ backend/config/bootstrap.php
@@ -2,5 +2,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
